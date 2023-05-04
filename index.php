f5a11af15c56c8d7a338f7d73bdd573f2d8560c7
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-05-04 18:32:49 +0200

    Add: table with data

--- index.php
+++ index.php
@@ -37,3 +37,46 @@
     ],
   ];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <title>Hotel</title>
+</head>
+<body>
+  <h1 class="text-center">Hotel</h1>
+  <div class="container">
+    <table class="table table-striped">
+      <thead class="text-center">
+        <tr>
+          <th>Nome</th>
+          <th>Descrizione</th>
+          <th>Parcheggio</th>
+          <th>Voto</th>
+          <th>Distanza dal centro</th>
+        </tr>
+      </thead>
+      <tbody class="text-center">
+        <?php foreach ($hotels as $hotel) { ?>
+          <tr>
+            <td><?php echo $hotel['name']; ?></td>
+            <td><?php echo $hotel['description']; ?></td>
+            <td><?php if ($hotel['parking'] === true) {
+              echo 'si';
+            } else {
+              echo 'no';
+            } ?>
+            </td>
+            <td><?php echo $hotel['vote']; ?></td>
+            <td><?php echo $hotel['distance_to_center']; ?> km</td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
+</body>
+</html>
